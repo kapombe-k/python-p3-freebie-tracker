2174f1fee078b8d4972f97301410a988a9d98538
Eugene Kapombe <kapombek@gmail.com>
2025-06-04 10:30:24 +0300

    added freebie class table

--- lib/models.py
+++ lib/models.py
@@ -2,12 +2,8 @@ from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -27,3 +23,12 @@ class Dev(Base):
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+class Freebie(Base):
+    __tablename__= 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    dev_id= Column(ForeignKey('devs.id'))
+    company_id=Column(ForeignKey('companies.id'))
